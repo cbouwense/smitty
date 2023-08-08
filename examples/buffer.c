86137b014920c774db3084ed4840b9792a5188e5
Christian Bouwense <bouwensec@gmail.com>
2023-08-08 02:02:18 +0000

    refactoring

--- examples/buffer.c
+++ examples/buffer.c
@@ -1,25 +1,17 @@
 #include "buffer.h"
 
 Buffer *buffer_new_default() {
-    Buffer *buffer = malloc(sizeof(Buffer));
-
-    buffer->data = malloc(BUFFER_DEFAULT_CAPACITY);
-    buffer->data_element_size = 1;
-    buffer->capacity = BUFFER_DEFAULT_CAPACITY;
-    buffer->read_cursor = buffer->data;
-    buffer->write_cursor = buffer->data;
-
-    return buffer;
+    return buffer_new(BUFFER_DEFAULT_CAPACITY);
 }
 
 Buffer *buffer_new(size_t capacity) {
     Buffer *buffer = malloc(sizeof(Buffer));
 
-    // buffer->data = malloc(capacity);
-    // buffer->data_element_size = 1;
+    buffer->data = malloc(capacity);
+    buffer->data_element_size = 1;
     buffer->capacity = capacity;
-    // buffer->read_cursor = buffer->data;
-    // buffer->write_cursor = buffer->data;
+    buffer->read_cursor = buffer->data;
+    buffer->write_cursor = buffer->data;
 
     return buffer;
 }
