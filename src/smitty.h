1e7251f846161e30ecb66b79f502447ef03a146b
Christian Bouwense <bouwensec@gmail.com>
2023-08-10 19:54:40 -0400

    cleanup

--- src/smitty.h
+++ src/smitty.h
@@ -49,25 +49,35 @@ typedef smitty_test_result (*smitty_test_case_func)();
         return 0; \
     }
 
-#define smitty_run_test_suite_with_before_each(before_each) \
+#define smitty_run_test_suite_with_before_each(before_each, ...) \
     int main() { \
+        smitty_test_case_func tests[] = { \
+            __VA_ARGS__ \
+            NULL \
+        }; \
         smitty_run_tests(tests, before_each, NULL); \
         return 0; \
     }
 
-#define smitty_run_test_suite_with_after_each(after_each) \
+#define smitty_run_test_suite_with_after_each(after_each, ...) \
     int main() { \
+        smitty_test_case_func tests[] = { \
+            __VA_ARGS__ \
+            NULL \
+        }; \
         smitty_run_tests(tests, NULL, after_each); \
         return 0; \
     }
 
-#define smitty_run_test_suite_with_before_and_after_each(before_each, after_each) \
+#define smitty_run_test_suite_with_before_and_after_each(before_each, after_each, ...) \
     int main() { \
+        smitty_test_case_func tests[] = { \
+            __VA_ARGS__ \
+            NULL \
+        }; \
         smitty_run_tests(tests, before_each, after_each); \
         return 0; \
     }
-
-#define smitty_test_debug_params const char *test_name, const char *file, const int line
 
 //--------------------------------------------------------------------------------------------------
 // Expects
@@ -81,13 +91,13 @@ typedef smitty_test_result (*smitty_test_case_func)();
 #define expect_non_null(actual)                failed_expect_count += expect_non_null_internal     (actual,           __func__, __FILE__, __LINE__) == EXPECT_FAIL ? 1 : 0;
 #define expect_pointer_equal(actual, expected) failed_expect_count += expect_pointer_equal_internal(actual, expected, __func__, __FILE__, __LINE__) == EXPECT_FAIL ? 1 : 0;
 
-smitty_expect_result expect_int_equal_internal(    const int   actual, const int   expected, smitty_test_debug_params);
-smitty_expect_result expect_string_equal_internal( const char *actual, const char *expected, smitty_test_debug_params);
-smitty_expect_result expect_true_internal(         const bool  actual,                       smitty_test_debug_params);
-smitty_expect_result expect_false_internal(        const bool  actual,                       smitty_test_debug_params);
-smitty_expect_result expect_null_internal(         const void *actual,                       smitty_test_debug_params);
-smitty_expect_result expect_non_null_internal(     const void *actual,                       smitty_test_debug_params);
-smitty_expect_result expect_pointer_equal_internal(const void *actual, const void *expected, smitty_test_debug_params);
+smitty_expect_result expect_int_equal_internal(    const int   actual, const int   expected, const char *test_name, const char *file, const int line);
+smitty_expect_result expect_string_equal_internal( const char *actual, const char *expected, const char *test_name, const char *file, const int line);
+smitty_expect_result expect_true_internal(         const bool  actual,                       const char *test_name, const char *file, const int line);
+smitty_expect_result expect_false_internal(        const bool  actual,                       const char *test_name, const char *file, const int line);
+smitty_expect_result expect_null_internal(         const void *actual,                       const char *test_name, const char *file, const int line);
+smitty_expect_result expect_non_null_internal(     const void *actual,                       const char *test_name, const char *file, const int line);
+smitty_expect_result expect_pointer_equal_internal(const void *actual, const void *expected, const char *test_name, const char *file, const int line);
 
 //--------------------------------------------------------------------------------------------------
 // Test runner core
