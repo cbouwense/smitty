ab1d897322431281a0b1126313d068feef08b5a8
Christian Bouwense <bouwensec@gmail.com>
2023-08-11 17:09:39 -0400

    remove non blackbox tests from scrump

--- examples/test_scrump.c
+++ examples/test_scrump.c
@@ -1,14 +1,6 @@
 #include "../src/smitty.h"
 #include "scrump.h"
 
-smitty_test(it_creates_a_default_buffer_with_a_non_null_pointer_to_its_data, {
-    ScrumpBuffer *buffer = scrump_buffer_create_default();
-
-    expect_non_null(buffer->data);
-
-    free(buffer);
-});
-
 smitty_test(it_returns_null_when_scrump_buffer_created_with_zero_capacity, {
     ScrumpBuffer *buffer = scrump_buffer_create(0);
 
@@ -24,28 +16,6 @@ smitty_test(it_returns_attempted_overflow_when_a_user_attempts_an_overflow, {
     ScrumpReturnCodeType result = scrump_buffer_write(buffer, data, strlen(data));
 
     expect_int_equal(result, SCRUMP_ATTEMPTED_WRITE_OVERFLOW);
-
-    free(buffer);
-});
-
-smitty_test(it_writes_the_data_to_the_buffer, {
-    ScrumpBuffer *buffer = scrump_buffer_create(1024);
-    char *data = "Hello, world!";
-
-    scrump_buffer_write(buffer, data, strlen(data));
-
-    expect_string_equal(buffer->data, data);
-
-    free(buffer);
-});
-
-smitty_test(it_writes_the_data_to_the_buffer_when_the_size_exactly_equals_remaining_capacity, {
-    ScrumpBuffer *buffer = scrump_buffer_create(13);
-    char *data = "Hello, world!";
-
-    scrump_buffer_write(buffer, data, strlen(data));
-
-    expect_string_equal(buffer->data, data);
 
     free(buffer);
 });
@@ -127,11 +97,8 @@ smitty_test(it_returns_data_when_the_user_tries_to_read_every_byte_written_so_fa
 
 // TODO: It would be nice to detect when a test has not been registered and print a warning.
 smitty_register_and_run_tests(
-    it_creates_a_default_buffer_with_a_non_null_pointer_to_its_data,
     it_returns_null_when_scrump_buffer_created_with_zero_capacity,
     it_returns_attempted_overflow_when_a_user_attempts_an_overflow,
-    it_writes_the_data_to_the_buffer,
-    it_writes_the_data_to_the_buffer_when_the_size_exactly_equals_remaining_capacity,
     it_returns_attempted_write_overflow_when_write_cursor_is_at_capacity,
     it_returns_attempted_read_overflow_when_user_attempts_to_read_past_the_write_cursor,
     it_returns_data_when_there_is_no_attempted_read_overflow,
